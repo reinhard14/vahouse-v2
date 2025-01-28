2fe027867933c5557d215d50c0bd959f8289d5c6
reinhard14 <reinhardangcan@gmail.com>
2025-01-28 22:40:22 +0800

    Update File uploads with remaining fixes and adding guidelines.

--- resources/views/components/applicant/guidelines.blade.php
+++ resources/views/components/applicant/guidelines.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-dialog-scrollable modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5">Guidelines</h1>
+                <h1 class="modal-title">Guidelines</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
--- resources/views/user/partials/file-uploads.blade.php
+++ resources/views/user/partials/file-uploads.blade.php
@@ -1,6 +1,14 @@
 <div class="row mb-4">
     <div class="col">
-        <label for="">Valid ID</label>
+        <div class="row">
+            <div class="col">
+                <label for="">Valid ID</label>
+            </div>
+            <div class="col text-right">
+                <a class="nav-link text-orange" href="#" data-bs-toggle="modal" data-bs-target="#guidelinesModal">Read Complete Guidelines</a>
+            </div>
+        </div>
+
         <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
         <small class="text-muted">Upload at least 2 Primary and Secondary Valid ID. See valid ID <a href="#">here</a> </small>
     </div>
@@ -17,6 +25,9 @@
     <div class="col">
         <label for="">DISC Result</label>
         <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
+        <small class="text-muted">Upload your latest DISC result. To take DISC assessment,
+            <a href="https://discpersonalitytesting.com/free-disc-test/" target="_blank"> click here</a>
+        </small>
     </div>
 </div>
 
@@ -31,6 +42,11 @@
     <div class="col">
         <label for="">Video Introduction</label>
         <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
+        <small class="text-muted">Upload at least 2 minute video introduction.
+            <a href="https://drive.google.com/file/d/1aJqNFMcso-kGSfjCYYmWg88-cWIkKu19/view" target="_blank">
+                Click here
+            </a> for a sample video introduction
+        </small>
     </div>
 </div>
 
@@ -38,6 +54,7 @@
     <div class="col">
         <label for="">Career/Skill Certifications</label>
         <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
+        <small class="text-muted">Please compile your certifications in .pdf file format </small>
     </div>
 </div>
 
@@ -45,6 +62,7 @@
 <div class="row mb-4">
     <div class="col">
         <label for="">Sample Mock Call</label>
+        <small class="d-block text-muted">Upload at least 2-minute mock-call each. <a href="#">Click here</a> for a sample mockcall</small>
         <span class="d-block">Inbound</span>
         <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
         <span class="d-block pt-3">Outbound</span>
@@ -52,3 +70,5 @@
 
     </div>
 </div>
+
+<x-applicant.guidelines />
