7ddb68010771c60e6ab850f06b8256459cd14124
reinhard14 <reinhardangcan@gmail.com>
2025-01-28 21:57:48 +0800

    Update the UI for File Uploads.

--- resources/views/user/partials/file-uploads.blade.php
+++ resources/views/user/partials/file-uploads.blade.php
@@ -1,1 +1,54 @@
-file-uploads partial
+<div class="row mb-4">
+    <div class="col">
+        <label for="">Valid ID</label>
+        <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
+        <small class="text-muted">Upload at least 2 Primary and Secondary Valid ID. See valid ID <a href="#">here</a> </small>
+    </div>
+</div>
+
+<div class="row mb-4">
+    <div class="col">
+        <label for="">CV or Resume</label>
+        <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
+    </div>
+</div>
+
+<div class="row mb-4">
+    <div class="col">
+        <label for="">DISC Result</label>
+        <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
+    </div>
+</div>
+
+<div class="row mb-4">
+    <div class="col">
+        <label for="">Portfolio</label>
+        <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
+    </div>
+</div>
+
+<div class="row mb-4">
+    <div class="col">
+        <label for="">Video Introduction</label>
+        <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
+    </div>
+</div>
+
+<div class="row mb-4">
+    <div class="col">
+        <label for="">Career/Skill Certifications</label>
+        <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
+    </div>
+</div>
+
+
+<div class="row mb-4">
+    <div class="col">
+        <label for="">Sample Mock Call</label>
+        <span class="d-block">Inbound</span>
+        <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
+        <span class="d-block pt-3">Outbound</span>
+        <input type="file" id="photo_id" name="photo_id" class="form-control" accept=".jpeg, .jpg, .png" required>
+
+    </div>
+</div>
