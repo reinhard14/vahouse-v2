e16b379b7d0c90c430ac12fb2b84579976c12dbe
reinhard14 <reinhardangcan@gmail.com>
2025-01-31 14:41:42 +0800

    View Profile User Interface.

--- resources/views/user/view-profile.blade.php
+++ resources/views/user/view-profile.blade.php
@@ -20,10 +20,11 @@
             </div>
             <div class="row mb-3">
                 <div class="col">
-                    <p class="mt-5 pt-5"> {{ $user->name }} {{ $user->middlename }} {{ $user->lastname }} </p>
-                    <h5> Jobs List here </h5>
+                    <span class="d-block mt-5 pt-5"> {{ $user->name }} {{ $user->middlename }} {{ $user->lastname }} </span>
+                    <span class="badge badge-pill badge-success span-normal-text">Active</span>
+                    <h5> Quality Assurance, Web Developer </h5>
                     <span class="text-orange"><i class="bi bi-patch-check-fill"></i> </span> <small class=""> Tier 1 VA </small>
-                    <span class="text-orange"><i class="bi bi-shield-fill-check"></i></span><small> HR Unverified</small>
+                    <span class="text-orange pl-3"><i class="bi bi-shield-fill-check"></i></span><small> HR Unverified</small>
                 </div>
             </div>
         </div>
@@ -34,15 +35,46 @@
         <div class="col-md-5 offset-md-3">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="text-muted">Overview</h5>
+                    <h5 class="text-muted mb-4">Overview</h5>
+                    <div class="row mb-2">
+                        <div class="col-md-1">
+                            <span class="badge badge-pill medium-icon p-2"><i class="bi bi-briefcase"></i></span>
+                        </div>
+                        <div class="col text-muted">
+                            Looking for <strong> full-time, night shift work</strong> for <strong>40 hours</strong> per week. <strong>35,000</strong> Pesos monthly salary.
+                        </div>
+                    </div>
 
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ullam minima dolorum ea vero obcaecati, beatae deleniti odio officia culpa debitis voluptatum quos adipisci saepe! Sequi, sapiente maiores hic suscipit voluptas eveniet, aliquam aperiam consequatur autem, et provident eaque totam!</p>
+                    <div class="row mb-3">
+                        <div class="col-md-1">
+                            <span class="badge badge-pill medium-icon p-2"><i class="bi bi-mortarboard"></i></span>
+                        </div>
+                        <div class="col text-muted">
+                            <strong>
+                                Bachelor of Science in Information Technology
+                            </strong>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-1">
+                            <span class="badge badge-pill medium-icon p-2"><i class="bi bi-person"></i></span>
+                        </div>
+                        <div class="col text-muted">
+                            <small class="d-block font-weight-bold d-flex align-end">
+                                Profile Created
+                            </small>
+                            <small class="d-flex align-start">
+                                Janaury 01, 2025
+                            </small>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div class="card">
                 <div class="card-body">
-                    <h5 class="text-muted">Profile Description</h5>
+                    <h5 class="text-muted mb-4">Profile Description</h5>
 
                     <p class="">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi doloribus odit nobis a, impedit laboriosam dignissimos deleniti quidem! Sit aliquid sapiente enim quidem. Fuga voluptatibus deserunt impedit vitae nostrum commodi hic quam, eum consequatur ullam!
@@ -54,21 +86,39 @@
                 <div class="card-body">
                     <h5 class="text-muted">Technical Skills</h5>
 
-                    <p class="">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, cumque!
-                    </p>
+                    <div class="mb-3">
+                        <span class="badge badge-pill badge span-orange">
+                            Web development
+                        </span>
+                        <span class="badge badge-pill badge span-orange">
+                            Web management
+                        </span>
+                    </div>
 
                     <h5 class="text-muted">Other Skills</h5>
 
-                    <p class="">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, cumque!
-                    </p>
+                    <div class="mb-3">
+                        <span class="badge badge-pill badge span-orange">
+                            Communication Skills
+                        </span>
+                    </div>
 
                     <h5 class="text-muted">Tools, Websites, and Apps Used</h5>
 
-                    <p class="">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, cumque!
-                    </p>
+                    <div class="mb-3">
+                        <span class="badge badge-pill badge span-orange">
+                            VS Code
+                        </span>
+                        <span class="badge badge-pill badge span-orange">
+                            Github
+                        </span>
+                        <span class="badge badge-pill badge span-orange">
+                            WAMP Server
+                        </span>
+                        <span class="badge badge-pill badge span-orange">
+                            Postman
+                        </span>
+                    </div>
                 </div>
             </div>
 
@@ -113,9 +163,93 @@
         <div class="col-md-3">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="text-muted">VA Information</h5>
+                    <h5 class="text-muted mb-3">VA Information</h5>
 
-                    <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad suscipit repellendus architecto quo nisi? Laudantium libero sit placeat sed quaerat distinctio quidem consequuntur quas voluptas minus ut odit nobis eius rerum adipisci nesciunt, recusandae qui similique eligendi alias asperiores veritatis nisi aliquam saepe? Doloremque ab aut aperiam, saepe quam quae cumque! Laudantium nesciunt unde, rerum reprehenderit ipsum sapiente sequi omnis error eveniet cupiditate aliquid eius aliquam beatae provident voluptatum qui culpa inventore accusamus possimus explicabo rem dolorem nulla iure voluptates. Obcaecati maxime fugit, ad quis ab iure inventore minima vero suscipit doloremque facere eum praesentium culpa quibusdam nam excepturi error delectus saepe. Placeat ut doloribus velit aliquid aspernatur sint ad, nesciunt quidem mollitia ratione facilis. Iste illo quaerat fugit id temporibus quos hic perspiciatis provident. Nostrum doloremque, possimus quibusdam deleniti totam unde? Sit possimus iste deleniti rem voluptatibus similique culpa, sunt cumque veniam, deserunt obcaecati ratione magnam esse reiciendis quibusdam?</p>
+                    <small class="text-muted">Age</small>
+                    <p class="text-muted font-weight-bold">
+                        20
+                    </p>
+
+                    <small class="text-muted">Gender</small>
+                    <p class="text-muted font-weight-bold">
+                        Male
+                    </p>
+
+                    <small class="text-muted">Address</small>
+                    <p class="text-muted font-weight-bold">
+                        440 Rizal St., Padada, Davao del Sur
+                    </p>
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <small class="text-muted d-block">Files:</small>
+                            <span class="d-block text-muted font-weight-bold">Portfolio</span>
+                            <small class="font-weight-bold text-orange">
+                                https://www.youtube.com/watch?v=dQw4w9WgXcQ
+                            </small>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <span class="d-block text-muted font-weight-bold">DISC</span>
+                            <small class="font-weight-bold text-orange">
+                                Adil.pdf
+                            </small>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <span class="d-block text-muted font-weight-bold">Intro Video</span>
+                            <small class="font-weight-bold text-orange">
+                                https://www.youtube.com/watch?v=dQw4w9WgXcQ
+                            </small>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <span class="d-block text-muted font-weight-bold">Mock Call</span>
+                            <small class="font-weight-bold text-orange">
+                                Inbound.mp3
+                            </small>
+                            <small class="font-weight-bold text-orange">
+                                Outbound.mp3
+                            </small>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col">
+                            <span class="d-block text-muted font-weight-bold">Valid ID</span>
+                            <small class="font-weight-bold text-orange">
+                                AdilID.pdf
+                            </small>
+                        </div>
+                    </div>
+
+                    <small class="text-muted">Contacts</small>
+                    <span class="d-block text-muted font-weight-bold">
+                        Skype
+                    </span>
+                    <small class="text-muted">reinhardangcan</small>
+
+                    <span class="d-block text-muted font-weight-bold">
+                        Email
+                    </span>
+                    <small class="text-muted">dwc.fs.dummy@gmail.com</small>
+
+                    <span class="d-block text-muted font-weight-bold">
+                        Phone Number
+                    </span>
+                    <small class="text-muted">09200930930</small>
+
+                    <small class="d-block text-muted mt-4">Bank Account</small>
+                    <span class="d-block text-muted font-weight-bold">
+                        UnionBank of the Philippines
+                    </span>
+                    <small class="text-muted">9900930930</small>
                 </div>
             </div>
 
@@ -123,9 +257,18 @@
                 <div class="card-body">
                     <h5 class="text-muted">Emergency Contact Information</h5>
 
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil corrupti molestias dignissimos, odit autem voluptatum inventore magni perferendis quis exercitationem quo facilis veniam esse corporis, cum ut repellat eius eveniet.
+                    <small class="text-muted d-block">
+                        Incase of Emergency
+                    </small>
+                    <p class="text-muted font-weight-bold">
+                        Henry Sy
                     </p>
+                    <small class="text-muted d-block font-weight-bold">
+                        Father
+                    </small>
+                    <small class="text-muted d-block font-weight-bold">
+                        09200920920
+                    </small>
                 </div>
             </div>
         </div>
