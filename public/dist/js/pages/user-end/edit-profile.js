9d6080163da53cee041a411a2b27dd5f78ec3dcf
reinhard14 <reinhardangcan@gmail.com>
2025-03-05 00:57:30 +0800

    Update JS for personal details-- edit-profile.

--- public/dist/js/pages/user-end/edit-profile.js
+++ public/dist/js/pages/user-end/edit-profile.js
@@ -23,4 +23,17 @@ $(document).ready(function() {
     $('#position7').change(function () {
         $('#specify').prop('disabled', !this.checked);
     }).trigger('change');
+
+    $('#skype').on('keyup', () => {
+        mappedWords = ['NA',  'N/A', 'n/a', 'na', 'none', 'None', 'NONE',
+                     'not available', 'not applicable', 'Not applicable',
+                     'NOT APPLICABLE', 'NOT AVAILABLE', 'Not Available'];
+
+        for (var index = 0; index < mappedWords.length; index++) {
+            if (skype.value === mappedWords[index]) {
+                invalidSkype();
+            }
+        }
+
+    });
 });
