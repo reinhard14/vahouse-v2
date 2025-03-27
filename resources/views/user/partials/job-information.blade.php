ffdd69c35045af0ee52966f72d050dabedfbf6cf
reinhard14 <reinhardangcan@gmail.com>
2025-03-27 20:51:40 +0800

    null on negotiable.

--- resources/views/user/partials/job-information.blade.php
+++ resources/views/user/partials/job-information.blade.php
@@ -148,6 +148,9 @@
 
             <div class="row mt-3">
                 <div class="col">
+                    @php
+                        $negotiable = optional($user->information)->negotiable;
+                    @endphp
                     <input type="checkbox" id="negotiable" name="negotiable" class="formCheckInput" value="yes"
                         {{ ($user->information->negotiable==="yes") ? 'checked' : '' }} >
                     <label for="negotiable" class="custom-label">  Salary is negotiable? <span class="text-muted">(Check if yes)</span></label>
